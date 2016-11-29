df2e57bee75bf88bf6d55403e3afb08432c38dd9
Akshay Oommen <akshay_oommen@apple.com>
2016-11-29 22:00:05 +0530

    Added carousel directive + updated Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # sample-project-A
+
+Steps to setup
+--------------
+
+1. Install the latest version of nodeJS.
+2. Once installed, run 'npm -v' to check the version number and verify that it is installed.
+3. Install bower using the command 'npm bower install -g' (omit -g if you do not wish for a global install)
+4. Install yaxy using the command 'npm yaxy install -g' (omit -g if you do not wish for a global install)
+5. Now take a git clone of the carousel sample-project repository using 'git clone https://github.com/AkshayOommen/sample-project-A.git' or 'git clone git@github.com:AkshayOommen/sample-project-A.git'
+6. Done? Great. Now open up command prompt (or Terminal if you're using a Mac) and set current working directory to sample-project-A
+7. Once your current working directory is set, run 'bower install' to install all the JS library dependancies of the project.
+8. Then, open up yaxy-config.txt and provide the project directory path that needs to be accessed at the mentioned url 'in this case, localhost:8081' (More info on yaxy: https://github.com/Kolyaj/Yaxy)
+9. Done? Great. Run the command 'yaxy' in the same path where your yaxy-config.txt is present.
+10. Finally, run Chrome through Terminal on a Mac (/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --proxy-server=localhost:8558) or Command Prompt on Windows (google-chrome --proxy-server="localhost:8558") to hit the proxy server.
+11. Hit the url 'http://localhost:8081/app/#/carousel-view'
--- app/scripts/CarouselViewComponents.js
+++ app/scripts/CarouselViewComponents.js
@@ -10,75 +10,148 @@
 var app = angular.module('sample-project');
 
 app.controller('CarouselViewController', ['$http', '$scope', '$timeout', function ($http, $scope, $timeout) {
-    var videoCarousel,
-        overlayContent,
-        video;
-    /**
-     * @description This method is called when the user presses the 'Esc' key while viewing a video
-     */
-    $scope.exitVideoPlay = function (event) {
-        if (event.keyCode === 27) {
-            $scope.isVideoSelected = false;
-
-            video.trigger('pause');
-            video[0].currentTime = 0;
-            videoCarousel.focus();
-        }
-    };
-
-    /**
-     * @description This method is called on initialization of the controller
-     */
+    //TODO
     $scope.init = function () {
-        videoCarousel = $('.video-carousel');
-        overlayContent = $('.content');
-        video = $('video');
-
-        $http.get('../app/scripts/videoList.json').then(function (res) {
-            $scope.videoList = res.data.entries || [];
-            $scope.activeIndex = 0;
-            $scope.selectedVideo = $scope.videoList[0];
-            videoCarousel.focus();
-        });
-    };
-
-    /**
-     * @description This method is called whenever the user tries to navigate through the carousel with the left and right arrows
-     */
-    $scope.navigate = function(event) {
-        $scope.width = 223;
-        if (event.keyCode === 39) {
-            $scope.activeIndex = $scope.activeIndex === ($scope.videoList.length - 1) ? 0 : ($scope.activeIndex + 1);
-            videoCarousel.animate({ scrollLeft: $scope.width * $scope.activeIndex}, "fast");
-        } else if (event.keyCode === 37) {
-            $scope.activeIndex = $scope.activeIndex === 0 ? ($scope.videoList.length - 1) : ($scope.activeIndex - 1);
-            videoCarousel.animate({ scrollLeft: $scope.width * $scope.activeIndex}, "fast");
-        } else if (event.keyCode === 13) {
-            $scope.playVideo();
-        }
-
-        $scope.selectedVideo = $scope.videoList[$scope.activeIndex];
-    };
-
-    /**
-     * @description This method is called when the user clicks on the 'Play video' button or the user presses enter when a video is selected in the carousel
-     */
-    $scope.playVideo = function () {
-        $scope.isVideoSelected = true;
-        $timeout(function () {
-            overlayContent.focus();
-        }, 100);
-    };
-
-    /**
-     * @description This method is called when the user manually selects a video off the carousel
-     */
-    $scope.selectVideo = function(index) {
-        $scope.activeIndex = index;
-        $scope.selectedVideo = $scope.videoList[$scope.activeIndex];
+        $scope.info = {
+            playList: []
+        };
+        $scope.showHistory = false;
     };
 
     $scope.init();
+}]);
+
+/**
+ * @description a directive to render a carousel view containing scope.video clips
+ * @author Akshay Abraham
+ *
+ * @usage <carousel ng-modal="obj-array" ng-show="boolean" json-path="json" response-name="string"/>
+ */
+app.directive('carousel',['$http', '$log', '$timeout', function($http, $log, $timeout) {
+    return {
+        restrict: 'E',
+        scope: {
+            ngModel: '=',
+            jsonPath: '@',
+            responseName: '@',
+            playList: '='
+        },
+        templateUrl: 'scripts/carousel-template.html',
+        link: function(scope, elm, attrs, controllers, transclude) {
+            /**
+             * @description This method is called when the user presses the 'Esc' key while viewing a scope.video
+             */
+            scope.keyboardVideoActions = function (event) {
+                if (event.keyCode === 27) {
+                    scope.isVideoSelected = false;
+
+                    scope.video.trigger('pause');
+                    scope.video[0].currentTime = 0;
+                    scope.videoCarousel.focus();
+                } else if (event.keyCode === 32) {
+                    scope.togglePlayer();
+                } else if (event.keyCode === 70) {
+                    scope.enterFullscreen();
+                }
+            };
+
+            /**
+             * @description This method is called on initialization of the controller
+             */
+            scope.init = function () {
+                scope.videoCarousel = $(elm).find('.video-carousel');
+                scope.overlayContent = $(elm).find('.content');
+                scope.video = $(elm).find('video');
+                scope.isPlay = false;
+                scope.isFullscreen = false;
+                scope.progressVal = 0;
+
+                scope.width = 219;
+
+                if (scope.jsonPath) {
+                    $http.get(scope.jsonPath).then(function (res) {
+                        scope.videoList = res.data[scope.responseName] || [];
+                        scope.activeIndex = 0;
+                        scope.selectedVideo = scope.videoList[0];
+                        scope.videoCarousel.focus();
+                    });
+                } else if (scope.playList) {
+                    scope.videoList = scope.playList;
+                    scope.activeIndex = 0;
+                    scope.selectedVideo = scope.videoList[0];
+                    scope.videoCarousel.focus();
+                } else {
+                    $log.error('Either jsonPath or dataList is required');
+                }
+            };
+
+            /**
+             * @description This method is called whenever the user tries to navigate through the carousel with the left and right arrows
+             */
+            scope.navigate = function(event) {
+                event.preventDefault();
+
+                if (event.keyCode === 39) {
+                    scope.activeIndex = scope.activeIndex === (scope.videoList.length - 1) ? 0 : (scope.activeIndex + 1);
+                    scope.videoCarousel.animate({ scrollLeft: event.target.clientWidth * Math.floor((scope.width * scope.activeIndex)/event.target.clientWidth)}, "fast");
+                } else if (event.keyCode === 37) {
+                    scope.activeIndex = scope.activeIndex === 0 ? (scope.videoList.length - 1) : (scope.activeIndex - 1);
+                    scope.videoCarousel.animate({ scrollLeft: event.target.clientWidth * Math.floor((scope.width * scope.activeIndex)/event.target.clientWidth)}, "fast");
+                } else if (event.keyCode === 13) {
+                    scope.playVideo();
+                }
+
+                scope.selectedVideo = scope.videoList[scope.activeIndex];
+            };
+
+            /**
+             * @description This method is called when the user clicks on the 'Play scope.video' button or the user presses enter when a scope.video is selected in the carousel
+             */
+            scope.playVideo = function () {
+                scope.isVideoSelected = true;
+
+                if (scope.ngModel) {
+                    scope.ngModel.push(scope.videoList[scope.activeIndex]);
+                }
+
+                $timeout(function () {
+                    scope.overlayContent.focus();
+                }, 100);
+            };
+
+            /**
+             * @description This method is called when the user manually selects a scope.video off the carousel
+             */
+            scope.selectVideo = function(index) {
+                scope.activeIndex = index;
+                scope.selectedVideo = scope.videoList[scope.activeIndex];
+                scope.videoCarousel.animate({ scrollLeft: event.target.clientWidth * Math.floor((scope.width * scope.activeIndex)/event.target.clientWidth)}, "fast");
+            };
+
+            /**
+            * @description This method plays or pauses the video
+            */
+            scope.togglePlayer = function(event) {
+                scope.isPlay = !scope.isPlay;
+                scope.video[0][scope.isPlay ? 'play' : 'pause']();
+            };
+
+            /**
+             * @description This method triggers fullscreen mode
+             */
+            scope.enterFullscreen = function(event) {
+                scope.video[0].webkitRequestFullscreen();
+            }
+
+            scope.init();
+
+            elm.find('video').on('timeupdate', function () {
+               scope.$apply(function () {
+                   scope.progressVal = Math.floor((scope.video[0].currentTime / scope.video[0].duration) * 100);
+               });
+            });
+        }
+    };
 }]);
 
 /**
